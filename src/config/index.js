e61700583ae0bf7a48d521bb3fe00d0551234a36
Kuris <kurisotaku@gmail.com>
2023-03-15 20:33:22 -0300

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
 
 //const URL_TEST = 'https://www.wswork.com.br/cars.json';
 //const URL_TEST = 'https://viacep.com.br/ws/RS/montenegro/ramiro/json';
-const URL_TEST = 'https://lf-telegrambot-users.herokuapp.com/cars';
+const URL_TEST = 'https://23bd-134-122-114-20.ngrok.io/categorias';
 
 export default {
   URL_BACKEND_TOP, URL_TEST,
